f124963fea30f0f9736d7b240fd42393c7d7ca52
ARUNAGIRINATHAN-K <arun31.march.2k6@gmail.com>
2025-08-22 12:36:40 +0530

    del-aca

---
+++ KR-connect/delete_academic_details.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include 'db_connect.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST['id'];
+
+    $sql = "DELETE FROM academic_details WHERE id=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+
+    if ($stmt->execute()) {
+        echo "success";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
