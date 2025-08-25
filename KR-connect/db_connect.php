86e89764a498170b7d533badbb392271cbf39aa0
ARUNAGIRINATHAN-K <arun31.march.2k6@gmail.com>
2025-08-25 11:26:18 +0530

    updated

---
+++ KR-connect/db_connect.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'localhost';
+$dbname = 'mic_database';
+$username = 'root';
+$password = '';
+$conn = new mysqli($host, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$conn->set_charset('utf8');
+?>
